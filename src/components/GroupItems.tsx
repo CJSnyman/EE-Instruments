2f48039793c3538b7f3cdf68e2fc881d934b2a3a
CJSnyman <craigsnyman200@gmail.com>
2024-06-25 21:01:01 +0200

    Instead of displaying the listings as json. It is now more readable and syntactically better.

--- src/components/GroupItems.tsx
+++ src/components/GroupItems.tsx
@@ -19,10 +19,16 @@ async function GroupItems({ region }: { region: string }) {
             <section>
                 {Object.entries(data).map((group, groupIndex) => (
                     <div key={groupIndex}>
-                        <h2>{group[0]}</h2>
-                        {group[1].map((listing, listingIndex) => (
-                            <p key={listingIndex}>{JSON.stringify(listing, null, 2)}</p>
-                        ))}
+                        <h2 className="text-3xl">{group[0]}</h2>
+                        {group[1].map((listing) => {
+                            const [listingName, listingType] = Object.entries(listing)[0];
+                            return (
+                                <div className="px-4 py-0.5 *:leading-tight">
+                                    <p>{listingName}</p>
+                                    <p className="text-gray-500 text-sm ps-3">{listingType}</p>
+                                </div>
+                            );
+                        })}
                     </div>
                 ))}
             </section>
